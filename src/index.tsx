6cc36d388ecd0da2f9cbb7b038cbe11be104ff03
郑俊鑫 <zhengjunxin.zjx@bytedance.com>
2024-04-08 15:57:44 +0800

    fix: 使用router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App></App>
+    }
+  ])
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router}></RouterProvider>
     </React.StrictMode>
 );
