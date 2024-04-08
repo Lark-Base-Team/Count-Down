36523eed97315f18cdbeeb5b4deca0f39e12ae7f
郑俊鑫 <zhengjunxin.zjx@bytedance.com>
2024-04-08 15:49:13 +0800

    fix: 使用hi

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import Chart from './Chart';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App></App>
-    },
-    {
-        path: '/chart',
-        element: <Chart></Chart>
-    }
-])
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}></RouterProvider>
+        <div>hi</div>
     </React.StrictMode>
 );
