6a60f15ae287f09225058eb6997af8032707478b
qiuxinping <qiuxinping@bytedance.com>
2024-05-14 21:28:15 +0800

    手动引入ud样式

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import SemiPlugin from "vite-plugin-semi-theme";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
     plugins: [
         react(),
-        SemiPlugin({
-            theme: "@semi-bot/semi-theme-universedesign",
-        }),
     ],
     server: {
         host: "0.0.0.0",
