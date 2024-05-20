0e899ecbf37ff305856144c42c7eab729212216a
qiuxinping <qiuxinping@bytedance.com>
2024-05-20 14:17:28 +0800

    删掉多余的依赖包

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import SemiPlugin from "vite-plugin-semi-theme";
 import { semiTheming } from "vite-plugin-semi-theming";
 
 // https://vitejs.dev/config/
@@ -8,12 +7,6 @@ export default defineConfig({
     base: "./",
     plugins: [
         react(),
-        // npm 使用这个来引入样式
-        // SemiPlugin({
-        //     theme: "@semi-bot/semi-theme-feishu-dashboard",
-        // }),
-
-        // 由于pnpm使用软链方式，所以pnpm需要使用这个插件
         semiTheming({
             theme: "@semi-bot/semi-theme-feishu-dashboard",
         }),
